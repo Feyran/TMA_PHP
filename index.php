bb393aa25de0228c42b5c786a63e3058a0f044a6
Rafal Fajkowski <rfajkowski@gmail.com>
2017-11-06 16:16:48 +0000

    Files added, function file, index file, includes

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+	require_once 'includes/functions.php'; // functions needed to run the webpage
+	include 'includes/header.php'; //* Header - contains opening body HTML tags, <head> section
+
+
+
+
+
+	include 'includes/footer.php' // Footer - contains closing body HTML tags
+?>
